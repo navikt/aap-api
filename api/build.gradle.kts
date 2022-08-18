5388f4354061daa349c2db160528359be50a552e
Robin <robin.tordly@nav.no>
2022-08-18 10:21:24 +0200

    basic ktor for starters

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -1,22 +1,29 @@
-val aapLibVersion = "3.1.11"
-val ktorVersion = "2.1.0"
-
 plugins {
     id("io.ktor.plugin")
     application
 }
 
 application {
-    mainClass.set("no.nav.aap.api.app.AppKt")
+    mainClass.set("AppKt")
 }
+
+val aapLibVersion = "3.1.11"
+val ktorVersion = "2.1.0"
 
 dependencies {
     implementation("com.github.navikt.aap-libs:ktor-utils:$aapLibVersion")
     implementation("com.github.navikt.aap-libs:kafka:$aapLibVersion")
+
+    implementation("io.ktor:ktor-server-core:$ktorVersion")
+    implementation("io.ktor:ktor-server-netty:$ktorVersion")
+    implementation("io.ktor:ktor-server-metrics-micrometer:$ktorVersion")
+    implementation("io.ktor:ktor-server-content-negotiation:$ktorVersion")
+
     implementation("ch.qos.logback:logback-classic:1.2.11")
     implementation("net.logstash.logback:logstash-logback-encoder:7.2")
+    implementation("io.micrometer:micrometer-registry-prometheus:1.9.2")
 
-    testImplementation(kotlin("test"))
     testImplementation("com.github.navikt.aap-libs:kafka-test:$aapLibVersion")
     testImplementation("io.ktor:ktor-server-test-host:$ktorVersion")
+    testImplementation(kotlin("test"))
 }
---
+++ api/main/Config.kt
@@ -0,0 +1,5 @@
+import no.nav.aap.kafka.streams.KStreamsConfig
+
+data class Config(
+    val kafka: KStreamsConfig,
+)
