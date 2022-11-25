08dc4d799eee9bf0ecce144a0d37137f3dc819d1
Richard Borge <richard.borge@nav.no>
2022-11-25 11:18:11 +0100

    Legger på dummyobjekt for å sjekke at path funker.

--- api/main/routes/VedtakRoutes.kt
+++ api/main/routes/VedtakRoutes.kt
@@ -7,14 +7,25 @@ import io.ktor.server.routing.*
 import io.ktor.server.util.*
 import no.nav.aap.dto.kafka.IverksettVedtakKafkaDto
 import no.nav.aap.kafka.streams.Store
+import java.time.LocalDate
+import java.util.*
 
 fun Routing.vedtak(vedtakStore: Store<IverksettVedtakKafkaDto>) {
+
+    val dummy = IverksettVedtakKafkaDto(
+        vedtaksid = UUID.randomUUID(),
+        innvilget = false,
+        grunnlagsfaktor = 0.0,
+        vedtaksdato = LocalDate.now(),
+        virkningsdato = LocalDate.now(),
+        fødselsdato = LocalDate.now()
+    )
 
     get("/vedtak/{personident}") {
         val personident: String = call.parameters.getOrFail("personident")
 
         when (val vedtak = vedtakStore[personident]) {
-            null -> call.respond(HttpStatusCode.NotFound)
+            null -> call.respond(HttpStatusCode.OK, dummy)
             else -> call.respond(HttpStatusCode.OK, vedtak)
         }
     }
