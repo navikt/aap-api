2df7baf2b103892710dcc47f456a0dca52d5af3a
Richard Borge <richard.borge@nav.no>
2023-09-19 14:01:44 +0200

    Logging
    Co-authored-by: Vetle Hollund <vetle.hollund@nav.no>

--- app/main/api/routes/VedtakRoutes.kt
+++ app/main/api/routes/VedtakRoutes.kt
@@ -12,8 +12,12 @@ import io.ktor.server.routing.*
 import io.ktor.server.util.*
 import no.nav.aap.dto.kafka.IverksettVedtakKafkaDto
 import no.nav.aap.kafka.streams.v2.StateStore
+import org.slf4j.LoggerFactory
+import java.lang.Exception
 import java.time.LocalDate
 import java.util.*
+
+private val logger = LoggerFactory.getLogger("VedtakRoutes")
 
 fun Routing.vedtak(arenaoppslagRestClient: ArenaoppslagRestClient) {
     //authenticate {
@@ -26,8 +30,14 @@ fun Routing.vedtak(arenaoppslagRestClient: ArenaoppslagRestClient) {
                 }
             }
         }) {
-            val request = call.receive<ArenaoppslagRequest>()
-            call.respond(arenaoppslagRestClient.hentVedtak(request))
+            try {
+                logger.info("Incomming")
+                val request = call.receive<ArenaoppslagRequest>()
+                call.respond(arenaoppslagRestClient.hentVedtak(request))
+            } catch(e: Exception) {
+                logger.error("Feil i kall", e)
+                call.respond(HttpStatusCode.InternalServerError)
+            }
         }
     //}
 }
