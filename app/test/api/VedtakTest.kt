9fbbd8200419127097d0ee0072e4228300f851e4
VHollund <vetlehoo@gmail.com>
2023-09-19 11:03:02 +0200

    fjernet kafka test siden kafka ikke brukes
    Co-authored-by: Richard Borge <richard.borge@nav.no>
    Co-authored-by: Sturle Helland <sturle.helland@nav.no>

--- app/test/api/VedtakTest.kt
+++ app/test/api/VedtakTest.kt
@@ -1,74 +1,12 @@
 package api
 
-import com.fasterxml.jackson.databind.SerializationFeature
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
-import com.nimbusds.jwt.SignedJWT
-import io.ktor.client.*
-import io.ktor.client.call.*
-import io.ktor.client.plugins.contentnegotiation.*
-import io.ktor.client.request.*
-import io.ktor.http.*
-import io.ktor.serialization.jackson.*
-import io.ktor.server.testing.*
-import api.kafka.Topics
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.runBlocking
-import no.nav.aap.dto.kafka.IverksettVedtakKafkaDto
-import no.nav.aap.kafka.streams.v2.test.TestTopic
-import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
-import java.time.LocalDate
-import java.util.*
 
 internal class VedtakRouteTest {
 
     @Test
-    fun `vedtak route accessible`() {
-        Mocks().use { mocks ->
-            lateinit var vedtakTopic: TestTopic<IverksettVedtakKafkaDto>
-
-            testApplication {
-                environment { config = mocks.environmentVariables }
-                application {
-                    api(mocks.kafka)
-                    vedtakTopic = mocks.kafka.testTopic(Topics.vedtak)
-                }
-
-                while (client.get("/actuator/ready").status != HttpStatusCode.OK) delay(10)
-
-                val client = createClient {
-                    install(ContentNegotiation) {
-                        jackson {
-                            registerModule(JavaTimeModule())
-                            disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
-                        }
-                    }
-                }
-
-
-                vedtakTopic.produce("1234") {
-                    IverksettVedtakKafkaDto(
-                        vedtaksid = UUID.fromString("ac9a1900-dbdc-4f89-b646-d8649a534e26"),
-                        innvilget = false,
-                        grunnlagsfaktor = 2.3,
-                        vedtaksdato = LocalDate.now(),
-                        virkningsdato = LocalDate.now(),
-                        fødselsdato = LocalDate.now().minusYears(37)
-                    )
-                }
-
-                assertEquals(UUID.fromString("ac9a1900-dbdc-4f89-b646-d8649a534e26"), client.getVedtak("/vedtak/1234", JwtGenerator::generateToken).vedtaksid)
-            }
-        }
+    fun `dummy test`() {
+        assert(true)
     }
 
-    private fun HttpClient.getVedtak(path: String, tokenSupplier: () -> SignedJWT): IverksettVedtakKafkaDto = runBlocking {
-        val response = get(path) {
-            bearerAuth(tokenSupplier().serialize())
-            accept(ContentType.Application.Json)
-        }
-
-        assertEquals(HttpStatusCode.OK, response.status)
-        response.body()
-    }
 }
