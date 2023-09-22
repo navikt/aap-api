7c06f71d157d8a07249d0dd6a7c78ebbe82e7146
VHollund <vetlehoo@gmail.com>
2023-09-22 11:01:56 +0200

    Fjernet ubrukt import som førte til feilet build
    Co-authored-by: Richard Borge <richard.borge@nav.no>

--- app/main/api/routes/ActuatorRoutes.kt
+++ app/main/api/routes/ActuatorRoutes.kt
@@ -6,7 +6,6 @@ import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import io.micrometer.prometheus.PrometheusMeterRegistry
-import no.nav.aap.kafka.streams.v2.Streams
 
 fun Routing.actuatorRoutes(
     prometheus: PrometheusMeterRegistry
