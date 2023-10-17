fa4b4e16de68cdfa67e285519296875298cfae6f
Richard Borge <richard.borge@nav.no>
2023-10-17 13:26:27 +0200

    Oppdaterer beskrivelse av callid

--- app/main/api/fellesordningen/VedtakParams.kt
+++ app/main/api/fellesordningen/VedtakParams.kt
@@ -3,4 +3,4 @@ package api.fellesordningen
 import com.papsign.ktor.openapigen.annotations.parameters.HeaderParam
 import java.util.UUID
 
-data class VedtakParams(@HeaderParam("Unik call id som brukes til logging") val `x-callid`: UUID)
+data class VedtakParams(@HeaderParam("Unik call id (uuid) som brukes til logging. Settes av den som kaller.") val `x-callid`: UUID)
