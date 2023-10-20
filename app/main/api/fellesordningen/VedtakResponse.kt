a307d59f6c0ef9822aa497fc35846cc3f94caf34
VHollund <vetlehoo@gmail.com>
2023-10-20 10:56:03 +0200

    til-dato kan være null
    Co-authored-by: Richard Borge <richard.borge@nav.no>
    Co-authored-by: Sturle Helland <sturle.helland@nav.no>

--- app/main/api/fellesordningen/VedtakResponse.kt
+++ app/main/api/fellesordningen/VedtakResponse.kt
@@ -4,4 +4,4 @@ import java.time.LocalDate
 
 data class VedtakResponse(val personId: String, val perioder: List<VedtakPeriode>)
 
-data class VedtakPeriode(val fraDato:LocalDate, val tilDato:LocalDate)
+data class VedtakPeriode(val fraDato:LocalDate, val tilDato:LocalDate?)
