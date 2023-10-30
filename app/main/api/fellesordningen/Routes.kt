0ffbf255661b34818aa1d2bb725b8b42eef95498
VHollund <vetlehoo@gmail.com>
2023-10-30 12:11:25 +0100

    bruker maskinporten igjen og logging gjenopptatt

--- app/main/api/fellesordningen/Routes.kt
+++ app/main/api/fellesordningen/Routes.kt
@@ -19,6 +19,7 @@ import java.time.LocalDateTime
 import java.util.*
 
 private val secureLog = LoggerFactory.getLogger("secureLog")
+private val logger = LoggerFactory.getLogger("App")
 private const val ORGNR = "987414502"
 private val objectMapper = jacksonObjectMapper()
     .disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
@@ -39,7 +40,13 @@ fun Route.fellesordningen(
             secureLog.error("Klarte ikke hente vedtak fra Arena", ex)
             throw ex
         }.onSuccess { res ->
-            call.respond(res)
+            try {
+                sporingsloggKafkaClient.sendMelding(lagSporingsloggEntry(body.personId, res))
+                call.respond(res)
+            } catch (e: Exception) {
+                secureLog.error("Klarte ikke produsere til kafka sporingslogg og kan derfor ikke returnere data", e)
+                call.respond(HttpStatusCode.ServiceUnavailable, "Feilet sporing av oppslag, kan derfor ikke returnere data. Feilen er på vår side, prøv igjen senere.")
+            }
         }
     }
 }
