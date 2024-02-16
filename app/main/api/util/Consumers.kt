b61eeb3016e45e97805fbb2313d26eca073440a0
Richard Borge <richard.borge@nav.no>
2024-02-16 10:36:40 +0100

    Legger inn ny konsument.

--- app/main/api/util/Consumers.kt
+++ app/main/api/util/Consumers.kt
@@ -8,16 +8,19 @@ object Consumers {
     private const val NAV_ORGNR = "889640782"
     private const val AFP_FELLERORDNINGEN_ORGNR = "987414502"
     private const val SPK_ORGNR = "982583462"
+    private const val GABLER = "916833520"
 
     private val consumerTags = mapOf(
         NAV_ORGNR to "NAV",
         AFP_FELLERORDNINGEN_ORGNR to "fellesordningen",
-        SPK_ORGNR to "SPK"
+        SPK_ORGNR to "SPK",
+        GABLER to "Gabler"
     )
 
     private val behandlingsgrunnlag = mapOf(
         AFP_FELLERORDNINGEN_ORGNR to "GDPR Art. 6(1)e. AFP-tilskottsloven §17 første ledd, §29 andre ledd, første punktum. GDPR Art. 9(2)b",
-        SPK_ORGNR to "GDPR Art. 6(1)e. AFP-tilskottsloven §17 første ledd, §29 andre ledd, første punktum. GDPR Art. 9(2)b"
+        SPK_ORGNR to "GDPR Art. 6(1)e. AFP-tilskottsloven §17 første ledd, §29 andre ledd, første punktum. GDPR Art. 9(2)b",
+        GABLER to "GDPR Art. 6(1)e. AFP-tilskottsloven §17 første ledd, §29 andre ledd, første punktum. GDPR Art. 9(2)b"
     )
 
     fun getConsumerTag(orgnr: String) = consumerTags[orgnr] ?: orgnr.also {
