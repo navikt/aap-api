f173095830cc9ed395a19858195f6c4c93fbac02
Fredrik Meyer <fredrik.meyer@nav.no>
2025-01-21 13:39:32 +0100

    Også logg path i callogging

--- app/main/api/util/CallLogging.kt
+++ app/main/api/util/CallLogging.kt
@@ -10,8 +10,9 @@ fun CallLoggingConfig.logging() {
         val status = call.response.status()
         val httpMethod = call.request.httpMethod.value
         val userAgent = call.request.headers["User-Agent"]
+        val path = call.request.path()
         val callId = call.request.header("x-callid") ?: call.request.header("nav-callId") ?: "ukjent"
-        "Status: $status, HTTP method: $httpMethod, User agent: $userAgent, callId: $callId"
+        "Status: $status, HTTP method: $httpMethod, User agent: $userAgent, callId: $callId, path: $path"
     }
     filter { call -> call.request.path().startsWith("/actuator").not() }
 }
