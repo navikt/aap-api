fcf3b01e2a31998613c93b27f1fde5e3c560b591
VHollund <vetle.hollund@nav.no>
2025-02-14 11:05:25 +0100

    oppdatering av kontrakt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,7 +47,7 @@ dependencies {
     implementation("io.ktor:ktor-client-jackson:$ktorVersion")
     implementation("io.ktor:ktor-client-logging:$ktorVersion")
 
-    implementation("no.nav.aap.arenaoppslag:kontrakt:0.0.16")
+    implementation("no.nav.aap.arenaoppslag:kontrakt:0.0.17")
 
     implementation("ch.qos.logback:logback-classic:1.5.16")
     implementation("com.auth0:java-jwt:4.5.0")
