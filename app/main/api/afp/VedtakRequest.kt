83f0c8b9894f6be686aa83b5fb011886a0112cc9
VHollund <vetle.hollund@nav.no>
2025-04-07 13:43:57 +0200

    require at fraogmed må være før eller lik tilogmed

--- app/main/api/afp/VedtakRequest.kt
+++ app/main/api/afp/VedtakRequest.kt
@@ -8,12 +8,18 @@ data class VedtakRequestMedSaksRef(
     val tilOgMedDato: LocalDate,
     val saksId: String
 ) {
+    init {
+        require(fraOgMedDato <= tilOgMedDato) { "fraOgMedDato må være før eller lik tilOgMedDato" }
+    }
     fun tilVedtakRequest() = VedtakRequest(personidentifikator, fraOgMedDato, tilOgMedDato)
 }
-
 
 data class VedtakRequest(
     val personidentifikator: String,
     val fraOgMedDato: LocalDate,
     val tilOgMedDato: LocalDate
-)
+) {
+    init {
+        require(fraOgMedDato <= tilOgMedDato) { "fraOgMedDato må være før eller lik tilOgMedDato" }
+    }
+}
