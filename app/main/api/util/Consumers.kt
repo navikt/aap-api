8806c9601d1e0ee37e1a7b1fe22dbbcdb07122a1
SelmaBergstrand <78426277+SelmaBergstrand@users.noreply.github.com>
2025-04-23 15:47:37 +0200

    Gi GFF tilgang i dev og prod (#377)

--- app/main/api/util/Consumers.kt
+++ app/main/api/util/Consumers.kt
@@ -14,6 +14,7 @@ object Consumers {
     private const val OSLO_PENSJONSFORSIKRING = "982759412"
     private const val ARENDAL = "940380014"
     private const val DRAMMEN = "980650383"
+    private const val GFF = "974652382"
 
     private val consumerTags = mapOf(
         NAV_ORGNR to "NAV",
@@ -24,7 +25,8 @@ object Consumers {
         STOREBRAND to "Storebrand",
         OSLO_PENSJONSFORSIKRING to "Oslo Pensjonsforsikring",
         ARENDAL to "Arendal Kommunale Pensjonskasse",
-        DRAMMEN to "Drammen Kommunale Pensjonskasse"
+        DRAMMEN to "Drammen Kommunale Pensjonskasse",
+        GFF to "GARANTIKASSEN FOR FISKERE"
     )
 
     private val behandlingsgrunnlag = mapOf(
@@ -35,7 +37,8 @@ object Consumers {
         STOREBRAND to "GDPR Art. 6(1)e. AFP-tilskottsloven §17 første ledd, §29 andre ledd, første punktum. GDPR Art. 9(2)b",
         OSLO_PENSJONSFORSIKRING to "GDPR Art. 6(1)e. AFP-tilskottsloven §17 første ledd, §29 andre ledd, første punktum. GDPR Art. 9(2)b",
         DRAMMEN to "GDPR Art. 6(1)e. AFP-tilskottsloven §17 første ledd, §29 andre ledd, første punktum. GDPR Art. 9(2)b",
-        ARENDAL to "GDPR Art. 6(1)e. AFP-tilskottsloven §17 første ledd, §29 andre ledd, første punktum. GDPR Art. 9(2)b"
+        ARENDAL to "GDPR Art. 6(1)e. AFP-tilskottsloven §17 første ledd, §29 andre ledd, første punktum. GDPR Art. 9(2)b",
+        GFF to "GDPR Art. 6(1)e. AFP-tilskottsloven §17 første ledd, §29 andre ledd, første punktum. GDPR Art. 9(2)b",
 
     )
 
