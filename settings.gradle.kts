61793e945b74de2408d052f1019edc8ff00df2d1
Fredrik Meyer <fredrik.meyer@nav.no>
2026-09-23 14:19:52 +0200

    AAP-BAU Bruk felles versjonkatalog (#772)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,6 +6,13 @@ include(
 
 dependencyResolutionManagement {
     // Felles for alle gradle prosjekter i repoet
+    dependencyResolutionManagement {
+        versionCatalogs {
+            create("kelvinLibs") {
+                from("no.nav.aap.kelvin:version-catalog:2.0.160")
+            }
+        }
+    }
     @Suppress("UnstableApiUsage")
     repositories {
         maven("https://github-package-registry-mirror.gc.nav.no/cached/maven-release")
